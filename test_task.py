9b6638d2a2c023bd0e09579b8ed2ce5831dc19b5
anonim767bn <anonim2462@yandex.ru>
2023-10-03 10:06:00 +0300

    add test

---
+++ test_task.py
@@ -0,0 +1,13 @@
+import pytest
+from task import task
+#r, t, a, v
+test_data = (
+    (1, 5, 5, 1, (267.47)),
+    (1, 1, 1, 1, (85.94)),
+    (2, 5, 10, 0, (340.99))
+)
+
+
+@pytest.mark.parametrize('r, t, a, v, expected', test_data)
+def test(r: float, t: float, a: float, v: float, expected):
+    assert task(radius=r, time=t, acceleration=a, start_velocity=v) == expected
